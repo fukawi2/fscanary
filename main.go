cb93fd52551fee8c576a815782a0578dc747877b
Phillip Smith <phillip.smith@naturesorganics.com.au>
2019-07-08 15:16:18 +1000

    split notifications into functions and add support for exclude patterns

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ import (
   "log"
   "os"
   "path"
+  "time"
   "github.com/rjeczalik/notify"
   "github.com/gobwas/glob"
   "gopkg.in/gomail.v2"
@@ -42,9 +43,16 @@ type watchPath struct {
   title   string    // name of the watch
   path    []string  // path(s) to watch
   pattern []string  // pattern(s) to match
+  exclude []string  // pattern(s) to exclude
   notify  bool      // should we notify for files that match?
   qtine   bool      // should we quarantine files that match?
   qdest   string    // destination to quarantine files to
+}
+type notification struct {
+  dest    string  // recipient of notification
+  subject string  // subject of notificaton
+  msg     string  // notification text
+  ts      int64   // timestamp notification was created
 }
 
 /*
@@ -53,6 +61,8 @@ type watchPath struct {
 var gconf global_config
 var watches []watchPath
 var version string  // set at build time by -ldflags
+var pending_notifs []notification
+
 func main() {
   // process command line arguments
   var conf_fname string
@@ -96,11 +106,11 @@ func main() {
   chNotify  := make(chan notify.EventInfo, 16)
   chQuit    := make(chan int)
 
-  fmt.Println("Creating watches. This may take a long time for large directory trees. Please wait for Ready message.")
+  logmsg(1, "Creating watches. This may take a long time for large directory trees. Please wait for Ready message.")
   for _,watch := range watches {
-    fmt.Println("Adding watch: ", watch.title)
+    logmsg(2, fmt.Sprintln("Adding watch: ", watch.title))
     for _,path := range watch.path {
-      fmt.Println("  Path: ", path)
+      logmsg(2, fmt.Sprintln("  Path: ", path))
       addWatch(chNotify, path)
     }
   }
@@ -117,12 +127,14 @@ func main() {
       logmsg(1, "QUIT")
       os.Exit(0)
     }
+    // async sending of pending_notifs
+    go send_pending_notifs()
   }
 }
 
-/*
+/******************************************************************************
  * FUNCTIONS
- */
+ *****************************************************************************/
 func addWatch(ch chan notify.EventInfo, path string) {
   if err := notify.Watch(path + string(os.PathSeparator) + "...", ch, notify.Create|notify.Write|notify.Rename); err != nil {
     log.Fatal(err)
@@ -154,34 +166,34 @@ func handle_event(ei notify.EventInfo) {
   fname := path.Base(ei.Path())
   for _,watch := range watches {
     // check for matching patterns
+    compare_paths:
     for _,pattern := range watch.pattern {
       logmsg(2, fmt.Sprintf("Comparing '%s' to '%s'", fname, pattern))
-      var g glob.Glob
-      g = glob.MustCompile(pattern)
-      if g.Match(fname) {
+      var g_path glob.Glob
+      g_path = glob.MustCompile(pattern)
+      if g_path.Match(fname) {
+        for _,exclude := range watch.exclude {
+          var g_exclude glob.Glob
+          g_exclude = glob.MustCompile(exclude)
+          if g_exclude.Match(fname) {
+            logmsg(5, fmt.Sprintf("File '%s' match exclude pattern '%s'; skipping", fname, exclude))
+            continue compare_paths
+          }
+        }
         logmsg(4, "Match found")
         if watch.notify {
-          // send notification
-          logmsg(1, fmt.Sprintf("Sending notification to '%s' due to file '%s'",
-            gconf.email, ei.Path()))
-          /*
-           * handle the sending of the email in a subroutine so it
-           * can run asynchronously and not hold up further processing
-           */
-          go func() {
-            myhostname,_ := os.Hostname()
-            m := gomail.NewMessage()
-            m.SetHeader("From", gconf.smtp_from)
-            m.SetHeader("To", gconf.email)
-            m.SetHeader("Subject",
-              fmt.Sprintf("File Matching '%s'", watch.title))
-            m.SetBody("text/plain",
-              fmt.Sprintf("The following file matching pattern '%s' was saved on host %s\n\n%s",
-              pattern, myhostname, ei.Path()))
-
-            d := gomail.Dialer{Host: gconf.smtp_server, Port: gconf.smtp_port}
-            if err := d.DialAndSend(m); err != nil { panic(err) }
-          }()
+          // generate a notification
+          myhostname,_ := os.Hostname()
+          logmsg(1, fmt.Sprintf("Generating notification for %s event on file '%s'",
+            event_verb, ei.Path()))
+          var n notification
+          n.dest = gconf.email
+          n.subject = fmt.Sprintf("File Matching '%s'", watch.title)
+          n.msg = fmt.Sprintf("The following file matching pattern '%s' was saved on host %s\n\n%s",
+                       pattern, myhostname, ei.Path())
+          t := time.Now().UTC()
+          n.ts = t.Unix()
+          pending_notifs = append(pending_notifs, n)
         }
         if watch.qtine {
           // quarantine the file
@@ -198,4 +210,20 @@ func handle_event(ei notify.EventInfo) {
     }
   }
   logmsg(3, fmt.Sprintf("Finished examining '%s'", ei.Path()))
+}
+
+func send_pending_notifs() {
+  curr_notifs := pending_notifs  // copy pending_notifs to a new slice
+  pending_notifs = nil          // erase the global pending_notifs slice
+  for _, n := range curr_notifs {
+    m := gomail.NewMessage()
+    m.SetHeader("From", gconf.smtp_from)
+    m.SetHeader("To", n.dest)
+    m.SetHeader("Subject", n.subject)
+    m.SetBody("text/plain", n.msg)
+
+    logmsg(1, fmt.Sprintf("Sending notification '%s' to '%s'", n.subject, n.dest))
+    d := gomail.Dialer{Host: gconf.smtp_server, Port: gconf.smtp_port}
+    if err := d.DialAndSend(m); err != nil { panic(err) }
+  }
 }
